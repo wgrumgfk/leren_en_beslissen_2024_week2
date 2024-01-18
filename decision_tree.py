2ee877e011afd3e5dd66864df41637c1e1ee9e70
Frederique7 <frederique.lalieu@gmail.com>
2024-01-18 18:06:29 +0100

    Predicts values and evaluates precision
    MSE is a little disappointing

--- decision_tree.py
+++ decision_tree.py
@@ -1,6 +1,9 @@
 from sklearn import tree
 import pandas as pd
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
+# Make sure all rows are complete
 df = pd.read_csv('okeanos_processed_decision_tree.csv')
 df.dropna(how = 'any', subset=['days_until_2k', 'man', 'zwaar','AT','I','ID','ED','aantal_intervallen','intervaltype','interval_afstand','interval_nummer', 'two_k_tijd_sec'], inplace=True)
 
@@ -8,7 +11,17 @@ df.dropna(how = 'any', subset=['days_until_2k', 'man', 'zwaar','AT','I','ID','ED
 X = df[['days_until_2k', 'man', 'zwaar','AT','I','ID','ED','500_split_sec','interval_afstand']]
 y = df[['two_k_tijd_sec']]
 
+# Split the data into training, validation, and testing sets
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.4, random_state=42)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+
+# Train the decision tree
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, y)
-predict = clf.predict([[88,0,0,0,0,1,0,390,4000]])
-print(predict)
+clf = clf.fit(X_train, y_train)
+
+#Predict the response for test dataset
+y_val_pred = clf.predict(X_val)
+
+# Evaluate performance
+mse_val = mean_squared_error(y_val, y_val_pred)
+print(f'Mean Squared Error on Validation Set: {mse_val}')
