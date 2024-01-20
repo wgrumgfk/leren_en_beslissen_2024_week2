5939c24dbc31a6b35f74151a263873756e70719a
Frederique7 <frederique.lalieu@gmail.com>
2024-01-20 17:04:09 +0100

    Added a file for my convenience and tried to visualize decision tree

--- decision_tree.py
+++ decision_tree.py
@@ -1,5 +1,6 @@
 from sklearn import tree
 import pandas as pd
+from sklearn import metrics
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
@@ -9,6 +10,7 @@ df.dropna(how = 'any', subset=['days_until_2k', 'man', 'zwaar','AT','I','ID','ED
 
 # Define features (X) and target variable (y)
 X = df[['days_until_2k', 'man', 'zwaar','AT','I','ID','ED','500_split_sec','interval_afstand']]
+feature_cols = ['days_until_2k', 'man', 'zwaar','AT','I','ID','ED','500_split_sec','interval_afstand']
 y = df[['two_k_tijd_sec']]
 
 # Split the data into training, validation, and testing sets
@@ -26,3 +28,21 @@ print(y_val_pred)
 # Evaluate performance
 mse_val = mean_squared_error(y_val, y_val_pred)
 print(f'Mean Squared Error on Validation Set: {mse_val}')
+
+# Model Accuracy, how often is the classifier correct?
+print("Accuracy:", metrics.accuracy_score(y_val, y_val_pred))
+
+from sklearn.tree import export_graphviz
+from io import StringIO
+# from sklearn.externals.six import StringIO  
+from IPython.display import Image  
+import pydotplus
+
+dot_data = StringIO()
+export_graphviz(clf, out_file=dot_data,  
+                filled=True, rounded=True,
+                special_characters=True,feature_names = feature_cols)
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+graph.write_png('diabetes.png')
+Image(graph.create_png())
+
