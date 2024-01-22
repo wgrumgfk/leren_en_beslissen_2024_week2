b1afb6197eaced542c6909cc0317484eb65fffba
Marijn Versluis <71717369+marijn0311@users.noreply.github.com>
2024-01-22 14:43:40 +0100

    Change comments and added todo

--- decision_tree.py
+++ decision_tree.py
@@ -10,9 +10,10 @@ df = pd.read_csv('okeanos_processed.csv')
 df.dropna(how = 'any', subset=['days_until_2k', 'man', 'zwaar','AT','I','ID','ED','aantal_intervallen','interval_afstand','interval_nummer', 'two_k_tijd_sec'], inplace=True)
 
 
-# Delete unecessary columns
+# Delete unecessary rows
 df.dropna(how = 'any', subset=['two_k_tijd_sec', 'days_until_2k', 'man', 'zwaar','AT','I','ID','ED','ED+','aantal_intervallen','afstand','interval_afstand','interval_nummer'], inplace=True)
 df['two_k_tijd_sec'] = df['two_k_tijd_sec'].multiply(10)
+#TODO: fix this to be 500_split_sec without error
 df['500_split_sec'] = df['two_k_tijd_sec'].multiply(10)
 df.round({'two_k_tijd_sec': 1})
 df['two_k_tijd_sec'] = df['two_k_tijd_sec'].astype(int)
