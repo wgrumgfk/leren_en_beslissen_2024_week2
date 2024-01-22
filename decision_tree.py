70590d38df8c647b33b0180f4f5b4e33e645b3dc
Marijn Versluis <71717369+marijn0311@users.noreply.github.com>
2024-01-22 14:21:32 +0100

    Removed imports and updated okeanos_processed

--- decision_tree.py
+++ decision_tree.py
@@ -3,8 +3,6 @@ import pandas as pd
 from sklearn import metrics
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-from sklearn.tree import export_graphviz
-import pydotplus
 from sklearn import tree
 
 # Make sure all rows are complete
