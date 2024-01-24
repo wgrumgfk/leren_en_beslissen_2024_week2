3213a9de43be606e5bfc2e133f4f5b4a177a9b76
Frederique7 <frederique.lalieu@gmail.com>
2024-01-24 13:19:39 +0100

    Some changes
    Apparently there were still some changes I didn't push

--- decision_tree.py
+++ decision_tree.py
@@ -8,7 +8,7 @@ from sklearn import tree
 from sklearn.tree import DecisionTreeRegressor
 
 regressor = False
-wattage = False
+wattage = True
 
 def watt_to_pace(watt):
     # if isinstance(watt, int):
@@ -36,7 +36,7 @@ X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, r
 # REGRESSOR #################################################################################################
 if regressor == True:
     # Train
-    regr = DecisionTreeRegressor(max_depth=10)
+    regr = DecisionTreeRegressor()
     regr.fit(X_train, y_train)
 
     # Predict
@@ -63,6 +63,9 @@ if regressor == False:
     y_train = y_train.multiply(10).astype(int)
     y_val = y_val.multiply(10).astype(int)
 
+    print(y_train)
+    print(y_val)
+
     # Train
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(X_train, y_train)
@@ -72,11 +75,15 @@ if regressor == False:
     y_val_pred = clf.predict(X_val)
 
     if wattage == True:
-        y_train_pred = numpy.array([watt_to_pace(x) for x in y_train_pred]) * 0.1
-        y_train = numpy.vectorize(watt_to_pace)(y_train) * 0.1
+        y_train = y_train * .1
+        y_train_pred = y_train_pred * .1
+        y_val = y_val * .1
+        y_val_pred = y_val_pred * .1
+        y_train_pred = numpy.array([watt_to_pace(x) for x in y_train_pred])
+        y_train = numpy.array([watt_to_pace(x) for x in y_train])
 
-        y_val_pred = numpy.array([watt_to_pace(x) for x in y_val_pred]) * 0.1
-        y_val = numpy.vectorize(watt_to_pace)(y_val) * 0.1
+        y_val_pred = numpy.array([watt_to_pace(x) for x in y_val_pred])
+        y_val = numpy.array([watt_to_pace(x) for x in y_val])
 
     else:
         y_train = y_train * .1
@@ -85,11 +92,13 @@ if regressor == False:
         y_val_pred = y_val_pred * .1
 
     # Evaluate
+    print(y_val)
+    print(y_val_pred)
     mse_train = mean_squared_error(y_train, y_train_pred)
     print("MSE train class:", mse_train)
     mse_test = mean_squared_error(y_val, y_val_pred)
     print("MSE val class:", mse_test)
 
     # This doesn't work yet for some reason
-    print("Accuracy train:",metrics.accuracy_score(y_train, y_train_pred))
-    print("Accuracy val:",metrics.accuracy_score(y_val, y_val_pred))
+    # print("Accuracy train:",metrics.accuracy_score(y_train, y_train_pred))
+    # print("Accuracy val:",metrics.accuracy_score(y_val, y_val_pred))
