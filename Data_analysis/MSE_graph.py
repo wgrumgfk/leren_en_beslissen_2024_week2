9a2e4db746e47a3f37bab51431d1b4997e404a3e
Frederique7 <frederique.lalieu@gmail.com>
2024-02-02 15:17:23 +0100

    I don't even know any more

--- Data_analysis/MSE_graph.py
+++ Data_analysis/MSE_graph.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 
 depth = [1,2,3,4,5,6,7,8,9,10]
-MSE_train = [257, 121, 75, 58, 44, 32, 23, 17, 12, 8]
-MSE_val = [257, 127, 81, 68, 59, 54, 53, 54, 57, 59]
+MSE_train = [256, 119, 76.3, 57.3, 39.8, 26.1, 16.1, 9.56, 5.67, 3.31]
+MSE_val = [263, 124, 83.4, 67.2, 52.0, 39.9, 32.4, 27.3, 24.6, 23.1]
 
 plt.plot(depth, MSE_train, label = 'MSE training data')
 plt.plot(depth, MSE_val, label = "MSE validation data")
@@ -12,5 +12,4 @@ plt.yticks([i for i in range(0,275,25)])
 plt.xlabel("Depth of regression tree")
 plt.ylabel("Mean squared error")
 plt.legend()
-plt.title('Depth of regression tree and mean squared error')
 plt.show()
--- iterating_decision_tree.py
+++ iterating_decision_tree.py
@@ -20,6 +20,13 @@ def watt_to_pace(watt):
     
 performance_dict = dict()
 mse_5 = []
+mse_5_baseline =[]
+vector_list = []
+vector_list_baseline = []
+
+model_feat= ['days_until_2k','man','zwaar','AT','I','ID','ED','rust_sec','afstand','ervaring','mean_500_per_training','aantal_intervallen', 'calculated_distance', 'two_k_watt']
+importances = {feature: [] for feature in model_feat[:-1]}
+best_importance = {feature: [] for feature in model_feat[:-1]}
 
 for iter in range(1, 101):
     rand_seed = random.randint(0, 100000)
@@ -33,7 +40,6 @@ for iter in range(1, 101):
     # For every model calculate the val_mse. Store the model with best val_mse.
     # Also print the test_mse for this model.
     for depth in (range(1,15)):
-        model_feat= ['days_until_2k','man','zwaar','AT','I','ID','ED','rust_sec','afstand','ervaring','500_split_watt','aantal_intervallen', 'calculated_distance', 'two_k_watt']
         
         # Drop all rows if any of the feature data is missing:
         model_feat_df = df.dropna(how = 'any', subset=model_feat, inplace=False)
@@ -69,28 +75,30 @@ for iter in range(1, 101):
         mse_train = mean_squared_error(y_train, y_train_pred)
         baseline_mse_train = mean_squared_error(numpy.array([watt_to_pace(baseline_prediction)  for _ in range(len(y_train))]), y_train)
 
-<<<<<<< Updated upstream
-        if depth == 5:  # Assuming you are interested in depth 7
-            residuals_val_sec = y_val - y_val_pred
-            if mse_val < min_val_mse:
-                min_val_mse = mse_val
-=======
         # Evaluate test
         y_test_pred = numpy.array([watt_to_pace(x) for x in y_test_pred])
         y_test = numpy.array([watt_to_pace(x)  for x in y_test])
         mse_test = mean_squared_error(y_test, y_test_pred)
         baseline_mse_test = mean_squared_error(numpy.array([watt_to_pace(baseline_prediction) for _ in range(len(y_test))]), y_test)
         
-        if depth == 5:  # Assuming you are interested in depth 7
+        if depth == 5:
             mse_5.append(mse_test)
+            mse_5_baseline.append(baseline_mse_test)
             residuals_test_sec = y_test - y_test_pred
+            residuals_test_baseline = y_test - numpy.array([watt_to_pace(baseline_prediction) for _ in range(len(y_test))])
+            vector_list.append(residuals_test_sec)
+            vector_list_baseline.append(residuals_test_baseline)
+            feature_importances = regr.feature_importances_
+            sorted_indices = numpy.argsort(feature_importances)[::-1]
+            for index in sorted_indices:
+                importances[X.columns[index]].append(feature_importances[index])
+            
             if mse_test < min_test_mse:
-                min_val_mse = mse_test
->>>>>>> Stashed changes
-                best_depth = depth
-                best_residuals = residuals_test_sec
-                best_iteration = iter
-        
+                best_model = regr
+                best_feature_importance = regr.feature_importances_
+                sorted_indices = numpy.argsort(best_feature_importance)[::-1]
+                for index in sorted_indices:
+                    best_importance[X.columns[index]] = best_feature_importance[index]
       
         if iter == 1:
             performance_dict[depth] = [[mse_val], [baseline_mse_val], [mse_val], [baseline_mse_val], [len(model_feat_df), model_feat]]
@@ -105,18 +113,45 @@ for iter in range(1, 101):
         #coefficients = pd.DataFrame({'Feature': X.columns, 'Coefficient': linear_reg_model.coef_})
         #print(coefficients)
 
-if best_residuals is not None:
-    plt.hist(best_residuals, bins=20)
-<<<<<<< Updated upstream
-    plt.title(f"Residuals (Validation Set) for the best model (Depth {best_depth})")
-=======
-    plt.title(f"Residuals test set")
->>>>>>> Stashed changes
-    plt.xlabel("Residuals")
-    plt.ylabel("Frequency")
-    plt.show()
+residuals = numpy.concatenate(vector_list, axis=None)
+print('mae', mean(abs(residuals)))
+
+accuracies = []
+for threshold in([1,2.5,5,7.5,10,12.5,15,17.7,20]):
+    succes = 0
+    for residual in residuals:
+        if abs(residual) <= threshold:
+            succes += 1
+    accuracies.append(succes / len(residuals))
+
+print(accuracies)
+
+plt.hist(residuals, weights=numpy.ones(len(residuals)) / len(residuals) * 100, bins=20)
+plt.xlabel("Residuals")
+plt.ylabel("Percentage")
+plt.show()
+
+residuals_baseline = numpy.concatenate(vector_list_baseline, axis=None)
+print('mae baseline', mean(abs(residuals_baseline)))
+
+accuracies_baseline = []
+for threshold in([1,2.5,5,7.5,10,12.5,15,17.7,20]):
+    succes = 0
+    for residual in residuals_baseline:
+        if abs(residual) <= threshold:
+            succes += 1
+    accuracies_baseline.append(succes / len(residuals_baseline))
+
+print(accuracies_baseline)
+
+plt.hist(residuals_baseline, weights=numpy.ones(len(residuals_baseline)) / len(residuals_baseline) * 100, bins = 20)
+plt.xlabel("Residuals")
+plt.ylabel("Percentage")
+plt.show()
+
 
 print(mean(mse_5))
+print(mean(mse_5_baseline))
 print('\n-------------------------')
 print('Model training and predicting done.\n')
 print("Printing model performances for ", iter, " iterations...\n")
@@ -135,14 +170,14 @@ for k,v in performance_dict.items():
     print("Baseline MSE train  :", sum(v[3])/len(v[3]))
     print("\n")
 
-"""
-# Print the best model and its mse's to screen.
-print("\nRESULTS:\nBest model nr: ", best_model_nr)
-print("validation MSE: ", best_val_mse)
-print("test MSE: ", best_test_mse)
-print('----------------------------')
-for k,v in performance_dict.items():
-    print("\nModel number ", k)
-    print("Validation MSE: ", v[0])
-    print("Test       MSE: ", v[1])
-"""
+mean_importances = {feature: numpy.mean(importance) for feature, importance in importances.items()}
+percentage_importances = {feature: (sum(importance > 0 for importance in importancess) / len(importancess)) * 100 if len(importancess) > 0 else 0 for feature, importancess in importances.items()}
+print(mean_importances)
+print(percentage_importances)
+print(best_importance)
+
+fig = plt.figure(figsize=(50,15))
+_ = tree.plot_tree(best_model, 
+                   feature_names = model_feat,
+                   filled=True)
+plt.show()
